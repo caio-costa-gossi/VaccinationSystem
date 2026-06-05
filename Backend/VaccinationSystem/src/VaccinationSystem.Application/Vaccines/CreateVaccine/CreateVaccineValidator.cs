3164e8086f5c7102bb22a4641d2e328ea5eea63c
CaioGossi <caio_gossi@hotmail.com>
2026-06-05 00:43:43 -0300

    feat: criar CreateVaccineValidator

---
+++ Backend/VaccinationSystem/src/VaccinationSystem.Application/Vaccines/CreateVaccine/CreateVaccineValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace VaccinationSystem.Application.Vaccines.CreateVaccine
+{
+    public class CreateVaccineValidator : AbstractValidator<CreateVaccineCommand>
+    {
+        public CreateVaccineValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty();
+        }
+    }
+}

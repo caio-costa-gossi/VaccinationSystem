603f384e4866bc88dc11a2c96d9d88a0cf57c1a0
CaioGossi <caio_gossi@hotmail.com>
2026-06-05 18:28:31 -0300

    refactor: assinalar default para ID de nova pessoa criada

--- Backend/VaccinationSystem/src/VaccinationSystem.Domain/Aggregates/Person.cs
+++ Backend/VaccinationSystem/src/VaccinationSystem.Domain/Aggregates/Person.cs
@@ -17,7 +17,7 @@ public class Person
         if (string.IsNullOrEmpty(name))
             throw new Exception("Nome é necessário");
 
-        Id = Guid.NewGuid();
+        Id = default;
         Name = name;
     }
 

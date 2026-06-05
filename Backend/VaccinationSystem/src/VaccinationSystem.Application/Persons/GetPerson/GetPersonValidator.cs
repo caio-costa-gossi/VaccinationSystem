2ac637cc7fa835539890c9fa84c99f7aa69cb555
CaioGossi <caio_gossi@hotmail.com>
2026-06-05 00:31:48 -0300

    feat: adicionar GetPersonValidator

---
+++ Backend/VaccinationSystem/src/VaccinationSystem.Application/Persons/GetPerson/GetPersonValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace VaccinationSystem.Application.Persons.GetPerson
+{
+    public class GetPersonValidator : AbstractValidator<GetPersonQuery>
+    {
+        public GetPersonValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty();
+        }
+    }
+}

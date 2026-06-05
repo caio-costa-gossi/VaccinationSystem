403dab8c3fe00980d8bf2b24b003a1547c20c81b
CaioGossi <caio_gossi@hotmail.com>
2026-06-05 00:40:40 -0300

    feat: criar DeleteVaccinationValidator

---
+++ Backend/VaccinationSystem/src/VaccinationSystem.Application/Vaccinations/DeleteVaccination/DeleteVaccinationValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace VaccinationSystem.Application.Vaccinations.DeleteVaccination
+{
+    public class DeleteVaccinationValidator : AbstractValidator<DeleteVaccinationCommand>
+    {
+        public DeleteVaccinationValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty();
+        }
+    }
+}

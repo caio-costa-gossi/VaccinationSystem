c344de857e8f20cfafb9abff8d9bae405e5510fa
CaioGossi <caio_gossi@hotmail.com>
2026-06-06 02:58:54 -0300

    feat: implementar Validators para use-cases de auth

---
+++ Backend/VaccinationSystem/src/VaccinationSystem.Application/Auth/LogIn/LoginValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace VaccinationSystem.Application.Auth.LogIn
+{
+    public class LoginValidator : AbstractValidator<LoginCommand>
+    {
+        public LoginValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(255)
+                .Must(name => !name.Contains(' '));
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MaximumLength(255)
+                .Must(pw => !pw.Contains(' '));
+        }
+    }
+}
---
+++ Backend/VaccinationSystem/src/VaccinationSystem.Application/Auth/RegisterUser/RegisterUserValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace VaccinationSystem.Application.Auth.RegisterUser
+{
+    public class RegisterUserValidator : AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(255)
+                .Must(name => !name.Contains(' '));
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MaximumLength(255)
+                .Must(pw => !pw.Contains(' '));
+        }
+    }
+}

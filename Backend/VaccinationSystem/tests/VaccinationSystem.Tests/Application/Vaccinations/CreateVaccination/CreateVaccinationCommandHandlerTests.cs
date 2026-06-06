981a0fd57ac33cedff2fb5e179e8ccbebf7a2057
CaioGossi <caio_gossi@hotmail.com>
2026-06-06 15:55:10 -0300

    feat: implementar testes unitários para use-case CreateVaccination

---
+++ Backend/VaccinationSystem/tests/VaccinationSystem.Tests/Application/Vaccinations/CreateVaccination/CreateVaccinationCommandHandlerTests.cs
@@ -0,0 +1,134 @@
+﻿using Moq;
+using VaccinationSystem.Application.Common.Exceptions;
+using VaccinationSystem.Application.Common.Interfaces;
+using VaccinationSystem.Application.Vaccinations.CreateVaccination;
+using VaccinationSystem.Domain.Aggregates;
+using VaccinationSystem.Domain.Common.Exceptions;
+
+namespace VaccinationSystem.Tests.Application.Vaccinations.CreateVaccination
+{
+    public class CreateVaccinationCommandHandlerTests
+    {
+        private readonly Mock<IPersonRepository> _personRepository;
+        private readonly Mock<IVaccineRepository> _vaccineRepository;
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+
+        private readonly CreateVaccinationCommandHandler _handler;
+
+        public CreateVaccinationCommandHandlerTests()
+        {
+            _personRepository = new();
+            _vaccineRepository = new();
+            _unitOfWork = new();
+
+            _handler = new(
+                _personRepository.Object,
+                _vaccineRepository.Object,
+                _unitOfWork.Object);
+        }
+
+        [Fact]
+        public async Task Handle_WhenPersonExistsVaccineExistsAndDoseIsValid_ShouldCreate()
+        {
+            // Arrange
+            Guid personId = Guid.NewGuid();
+            Guid vaccineId = Guid.NewGuid();
+
+            Person person = new("person");
+            Vaccine vaccine = new("vaccine");
+
+            _personRepository
+                .Setup(r => r.GetByIdAsync(personId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(person);
+
+            _vaccineRepository
+                .Setup(r => r.GetByIdAsync(vaccineId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(vaccine);
+
+            CreateVaccinationCommand command = new(personId, vaccineId, 1);
+
+            // Act
+            Guid newVaccination = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            _unitOfWork.Verify(u => u.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+
+            Assert.Single(person.Vaccinations);
+            Assert.Equal(Guid.Empty, person.Vaccinations.First().Id);
+            Assert.Equal(1, person.Vaccinations.First().DoseNumber);
+        }
+
+        [Fact]
+        public async Task Handle_WhenPersonExistsVaccineExistsAndDoseIsNotValid_ShouldThrow()
+        {
+            // Arrange
+            Guid personId = Guid.NewGuid();
+            Guid vaccineId = Guid.NewGuid();
+
+            Person person = new("person");
+            Vaccine vaccine = new("vaccine");
+
+            _personRepository
+                .Setup(r => r.GetByIdAsync(personId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(person);
+
+            _vaccineRepository
+                .Setup(r => r.GetByIdAsync(vaccineId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(vaccine);
+
+            CreateVaccinationCommand command = new(personId, vaccineId, 2);
+
+            // Act/Assert
+            await Assert.ThrowsAsync<BusinessRuleViolationException>(() =>
+                _handler.Handle(command, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task Handle_WhenPersonNotExistsAndVaccineExists_ShouldThrow()
+        {
+            // Arrange
+            Guid personId = Guid.NewGuid();
+            Guid vaccineId = Guid.NewGuid();
+
+            Vaccine vaccine = new("vaccine");
+
+            _personRepository
+                .Setup(r => r.GetByIdAsync(personId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync((Person?) null);
+
+            _vaccineRepository
+                .Setup(r => r.GetByIdAsync(vaccineId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(vaccine);
+
+            CreateVaccinationCommand command = new(personId, vaccineId, 2);
+
+            // Act/Assert
+            await Assert.ThrowsAsync<NotFoundException>(() =>
+                _handler.Handle(command, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task Handle_WhenPersonExistsAndVaccineNotExists_ShouldThrow()
+        {
+            // Arrange
+            Guid personId = Guid.NewGuid();
+            Guid vaccineId = Guid.NewGuid();
+
+            Person person = new("person");
+
+            _personRepository
+                .Setup(r => r.GetByIdAsync(personId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(person);
+
+            _vaccineRepository
+                .Setup(r => r.GetByIdAsync(vaccineId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync((Vaccine?) null);
+
+            CreateVaccinationCommand command = new(personId, vaccineId, 2);
+
+            // Act/Assert
+            await Assert.ThrowsAsync<NotFoundException>(() =>
+                _handler.Handle(command, CancellationToken.None));
+        }
+    }
+}
---
+++ Backend/VaccinationSystem/tests/VaccinationSystem.Tests/Application/Vaccinations/CreateVaccination/CreateVaccinationValidatorTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentValidation.Results;
+using VaccinationSystem.Application.Vaccinations.CreateVaccination;
+
+namespace VaccinationSystem.Tests.Application.Vaccinations.CreateVaccination
+{
+    public class CreateVaccinationValidatorTests
+    {
+        [Fact]
+        public void Validate_WhenPersonIdIsValidVaccineIdIsValidAndDoseNumberIsValid_ShouldBeTrue()
+        {
+            // Arrange
+            CreateVaccinationValidator validator = new();
+            CreateVaccinationCommand command = new(Guid.NewGuid(), Guid.NewGuid(), 1);
+
+            // Act
+            ValidationResult result = validator.Validate(command);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_WhenPersonIdIsEmpty_ShouldBeFalse()
+        {
+            // Arrange
+            CreateVaccinationValidator validator = new();
+            CreateVaccinationCommand command = new(Guid.Empty, Guid.NewGuid(), 1);
+
+            // Act
+            ValidationResult result = validator.Validate(command);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_WhenVaccineIdIsEmpty_ShouldBeFalse()
+        {
+            // Arrange
+            CreateVaccinationValidator validator = new();
+            CreateVaccinationCommand command = new(Guid.NewGuid(), Guid.Empty, 1);
+
+            // Act
+            ValidationResult result = validator.Validate(command);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_WhenDoseNumberIsNegative_ShouldBeFalse()
+        {
+            // Arrange
+            CreateVaccinationValidator validator = new();
+            CreateVaccinationCommand command = new(Guid.NewGuid(), Guid.NewGuid(), -1);
+
+            // Act
+            ValidationResult result = validator.Validate(command);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+    }
+}

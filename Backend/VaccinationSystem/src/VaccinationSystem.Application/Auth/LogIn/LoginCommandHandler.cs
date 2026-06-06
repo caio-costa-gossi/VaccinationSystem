8f08842a77538834c0a8ed05bee3b467ddb9d121
CaioGossi <caio_gossi@hotmail.com>
2026-06-05 23:09:08 -0300

    feat: implementar use-case LogIn
    - Implementar LoginCommandHandler
    - Implementar exceção UnauthorizedException

--- Backend/VaccinationSystem/src/VaccinationSystem.Application/Auth/LogIn/LoginCommandHandler.cs
+++ Backend/VaccinationSystem/src/VaccinationSystem.Application/Auth/LogIn/LoginCommandHandler.cs
@@ -1,4 +1,6 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using VaccinationSystem.Application.Common.Exceptions;
 using VaccinationSystem.Application.Common.Interfaces;
 using VaccinationSystem.Domain.Auth;
 
@@ -6,17 +8,28 @@ namespace VaccinationSystem.Application.Auth.LogIn
 {
     public class LoginCommandHandler(
         IUserRepository userRepository,
-        IJwtTokenGenerator tokenGenerator) 
+        IJwtTokenGenerator tokenGenerator,
+        IPasswordHasher<User> passwordHasher) 
         : IRequestHandler<LoginCommand, LoginResponseDto>
     {
         private readonly IUserRepository _userRepository = userRepository;
         private readonly IJwtTokenGenerator _tokenGenerator = tokenGenerator;
+        private readonly IPasswordHasher<User> _passwordHasher = passwordHasher;
 
         public async Task<LoginResponseDto> Handle(LoginCommand request, CancellationToken cancellationToken)
         {
-            return new LoginResponseDto(
-                _tokenGenerator.GenerateToken(
-                    new User { Id = Guid.NewGuid(), Name = "João", Password = "123" }));
+            User? user = await _userRepository.GetByNameAsync(request.Name, cancellationToken);
+
+            if (user == null)
+                throw new UnauthorizedException("Nome de usuário ou senha estão incorretos.");
+
+            PasswordVerificationResult result = 
+                _passwordHasher.VerifyHashedPassword(user, user.Password, request.Password);
+
+            if (result == PasswordVerificationResult.Failed)
+                throw new UnauthorizedException("Nome de usuário ou senha estão incorretos.");
+
+            return new LoginResponseDto(_tokenGenerator.GenerateToken(user));
         }
     }
 }
---
+++ Backend/VaccinationSystem/src/VaccinationSystem.Application/Common/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,8 @@
+﻿namespace VaccinationSystem.Application.Common.Exceptions
+{
+    internal class UnauthorizedException : Exception
+    {
+        public UnauthorizedException(string message)
+            : base(message) { }
+    }
+}

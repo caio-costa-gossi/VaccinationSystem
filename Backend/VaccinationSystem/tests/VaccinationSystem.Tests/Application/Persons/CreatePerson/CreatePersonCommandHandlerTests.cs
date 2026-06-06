8cc7d82f69f512a968e2a00bb0847c007620fcc3
CaioGossi <caio_gossi@hotmail.com>
2026-06-06 14:45:56 -0300

    feat: criar testes unitários para use-case CreatePerson

---
+++ Backend/VaccinationSystem/tests/VaccinationSystem.Tests/Application/Persons/CreatePerson/CreatePersonCommandHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using VaccinationSystem.Application.Common.Interfaces;
+using VaccinationSystem.Application.Persons.CreatePerson;
+
+namespace VaccinationSystem.Tests.Application.Persons.CreatePerson
+{
+    public class CreatePersonCommandHandlerTests
+    {
+        private readonly Mock<IPersonRepository> _personRepository;
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+
+        private readonly CreatePersonCommandHandler _handler;
+
+        public CreatePersonCommandHandlerTests()
+        {
+            _personRepository = new();
+            _unitOfWork = new();
+
+            _handler = new(
+                _personRepository.Object,
+                _unitOfWork.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldCreatePerson()
+        {
+            // Arrange
+            CreatePersonCommand command = new("person");
+
+            // Act
+            Guid response = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.Equal(Guid.Empty, response);
+            _unitOfWork.Verify(u => u.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+}
---
+++ Backend/VaccinationSystem/tests/VaccinationSystem.Tests/Application/Persons/CreatePerson/CreatePersonValidatorTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation.Results;
+using VaccinationSystem.Application.Persons.CreatePerson;
+
+namespace VaccinationSystem.Tests.Application.Persons.CreatePerson
+{
+    public class CreatePersonValidatorTests
+    {
+        [Fact]
+        public void Validate_WhenNameIsValid_ShouldBeTrue()
+        {
+            // Arrange
+            CreatePersonValidator validator = new();
+            CreatePersonCommand command = new("person");
+
+            // Act
+            ValidationResult result = validator.Validate(command);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_WhenNameIsEmpty_ShouldBeFalse()
+        {
+            // Arrange
+            CreatePersonValidator validator = new();
+            CreatePersonCommand command = new("  ");
+
+            // Act
+            ValidationResult result = validator.Validate(command);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_WhenNameIsTooBig_ShouldBeFalse()
+        {
+            // Arrange
+            CreatePersonValidator validator = new();
+            CreatePersonCommand command = new(new('a', 256));
+
+            // Act
+            ValidationResult result = validator.Validate(command);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+    }
+}

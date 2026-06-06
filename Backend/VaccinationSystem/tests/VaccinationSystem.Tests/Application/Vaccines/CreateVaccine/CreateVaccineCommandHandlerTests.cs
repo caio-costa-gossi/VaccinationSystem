186ff8b34be5d2cb6cf5fc30d7705877102e3731
CaioGossi <caio_gossi@hotmail.com>
2026-06-06 16:12:38 -0300

    feat: implementar testes unitário para use-case CreateVaccine

---
+++ Backend/VaccinationSystem/tests/VaccinationSystem.Tests/Application/Vaccines/CreateVaccine/CreateVaccineCommandHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using VaccinationSystem.Application.Common.Interfaces;
+using VaccinationSystem.Application.Vaccines.CreateVaccine;
+
+namespace VaccinationSystem.Tests.Application.Vaccines.CreateVaccine
+{
+    public class CreateVaccineCommandHandlerTests
+    {
+        private readonly Mock<IVaccineRepository> _vaccineRepository;
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+
+        private readonly CreateVaccineCommandHandler _handler;
+
+        public CreateVaccineCommandHandlerTests()
+        {
+            _vaccineRepository = new();
+            _unitOfWork = new();
+
+            _handler = new(
+                _vaccineRepository.Object,
+                _unitOfWork.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldCreate()
+        {
+            // Arrange
+            CreateVaccineCommand command = new("vaccine");
+
+            // Act
+            Guid newVaccineId = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            _unitOfWork.Verify(u => u.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+            Assert.Equal(Guid.Empty, newVaccineId);
+        }
+    }
+}
---
+++ Backend/VaccinationSystem/tests/VaccinationSystem.Tests/Application/Vaccines/CreateVaccine/CreateVaccineValidatorTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation.Results;
+using VaccinationSystem.Application.Vaccines.CreateVaccine;
+
+namespace VaccinationSystem.Tests.Application.Vaccines.CreateVaccine
+{
+    public class CreateVaccineValidatorTests
+    {
+        [Fact]
+        public void Validate_WhenNameIsValid_ShouldBeTrue()
+        {
+            // Arrange
+            CreateVaccineValidator validator = new();
+            CreateVaccineCommand command = new("vaccine");
+
+            // Act
+            ValidationResult result = validator.Validate(command);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_WhenNameIsEmpty_ShouldBeFalse()
+        {
+            // Arrange
+            CreateVaccineValidator validator = new();
+            CreateVaccineCommand command = new("");
+
+            // Act
+            ValidationResult result = validator.Validate(command);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_WhenNameIsTooBig_ShouldBeFalse()
+        {
+            // Arrange
+            CreateVaccineValidator validator = new();
+            CreateVaccineCommand command = new(new('a',256));
+
+            // Act
+            ValidationResult result = validator.Validate(command);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+    }
+}

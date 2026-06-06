6b9f98d9da5e60119f80ce6a0d4ef83448002d8d
CaioGossi <caio_gossi@hotmail.com>
2026-06-06 03:59:48 -0300

    feat: implementar testes unitários para use-case RegisterUser
    - RegisterUserCommandHandler
    - RegisterUserValidator
    - Adicionar package Moq

---
+++ Backend/VaccinationSystem/tests/VaccinationSystem.Tests/Application/Auth/RegisterUser/RegisterUserCommandHandlerTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Moq;
+using VaccinationSystem.Application.Auth.RegisterUser;
+using VaccinationSystem.Application.Common.Exceptions;
+using VaccinationSystem.Application.Common.Interfaces;
+using VaccinationSystem.Domain.Auth;
+
+namespace VaccinationSystem.Tests.Application.Auth.RegisterUser
+{
+    public class RegisterUserCommandHandlerTests
+    {
+        private readonly Mock<IUserRepository> _userRepository;
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+        private readonly Mock<IPasswordHasher<User>> _passwordHasher;
+
+        private readonly RegisterUserCommandHandler _handler;
+
+        public RegisterUserCommandHandlerTests()
+        {
+            _userRepository = new();
+            _unitOfWork = new();
+            _passwordHasher = new();
+
+            _handler = new(
+                _userRepository.Object, 
+                _unitOfWork.Object, 
+                _passwordHasher.Object);
+        }
+
+        [Fact]
+        public async Task Handle_WhenUserAndPasswordAreValidAndUserNotExists_ShouldRegister()
+        {
+            // Arrange
+            _userRepository
+                .Setup(r => r.UserNameExistsAsync("user", It.IsAny<CancellationToken>()))
+                .ReturnsAsync(false);
+
+            RegisterUserCommand command = new("user", "user");
+
+            // Act
+            await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            _unitOfWork.Verify(u => u.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Handle_WhenUserAndPasswordAreValidAndUserExists_ShouldThrow()
+        {
+            // Arrange
+            _userRepository
+                .Setup(r => r.UserNameExistsAsync("user", It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+
+            RegisterUserCommand command = new("user", "user");
+
+            // Act and Assert
+            await Assert.ThrowsAsync<ConflictException>(() =>
+                _handler.Handle(command, CancellationToken.None));
+        }
+    }
+}
---
+++ Backend/VaccinationSystem/tests/VaccinationSystem.Tests/Application/Auth/RegisterUser/RegisterUserValidatorTests.cs
@@ -0,0 +1,65 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using VaccinationSystem.Application.Auth.RegisterUser;
+
+namespace VaccinationSystem.Tests.Application.Auth.RegisterUser
+{
+    public class RegisterUserValidatorTests
+    {
+        [Fact]
+        public void Validate_WhenUserAndPasswordAreValid_ShouldBeTrue()
+        {
+            // Arrange
+            RegisterUserValidator validator = new();
+            RegisterUserCommand command = new("user", "user");
+
+            // Act
+            ValidationResult result = validator.Validate(command);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_WhenNameIsEmpty_ShouldBeFalse()
+        {
+            // Arrange
+            RegisterUserValidator validator = new();
+            RegisterUserCommand command = new("", "user");
+
+            // Act/Assert
+            ValidationResult result = validator.Validate(command);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_WhenPasswordContainsSpace_ShouldBeFalse()
+        {
+            // Arrange
+            RegisterUserValidator validator = new();
+            RegisterUserCommand command = new("user", "my password");
+
+            // Act/Assert
+            ValidationResult result = validator.Validate(command);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_WhenUserIsTooBig_ShouldBeFalse()
+        {
+            // Arrange
+            RegisterUserValidator validator = new();
+            RegisterUserCommand command = new(new('a', 256), "user");
+
+            // Act/Assert
+            ValidationResult result = validator.Validate(command);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+    }
+}

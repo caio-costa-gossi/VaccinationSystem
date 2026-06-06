6d3c2def3ae1b319af17e840d59ed81549dcb750
CaioGossi <caio_gossi@hotmail.com>
2026-06-05 23:36:27 -0300

    feat: Implementar geração de JWTs
    - Implementar JwtTokenGenerator
    - Adicionar package System.IdentityModel.Tokens.Jwt

--- Backend/VaccinationSystem/src/VaccinationSystem.Infrastructure/Auth/JwtTokenGenerator.cs
+++ Backend/VaccinationSystem/src/VaccinationSystem.Infrastructure/Auth/JwtTokenGenerator.cs
@@ -1,4 +1,8 @@
-﻿using VaccinationSystem.Application.Common.Interfaces;
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using VaccinationSystem.Application.Common.Interfaces;
 using VaccinationSystem.Domain.Auth;
 
 namespace VaccinationSystem.Infrastructure.Auth
@@ -9,7 +13,30 @@ namespace VaccinationSystem.Infrastructure.Auth
 
         public string GenerateToken(User user)
         {
-            return _config.Key;
+            // Criar claims
+            List<Claim> claims =
+            [
+                new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new(JwtRegisteredClaimNames.UniqueName, user.Name),
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            ];
+
+            // Criar assinatura
+            SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(_config.Key));
+            
+            SigningCredentials credentials = new(
+                key,
+                SecurityAlgorithms.HmacSha256);
+
+            // Criar token
+            JwtSecurityToken token = new(
+                issuer: _config.Issuer,
+                audience: _config.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_config.ExpirationMinutes),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }

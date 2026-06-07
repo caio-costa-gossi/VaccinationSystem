74feb53dd7c3519d8872196c683f28e0bccbce9c
CaioGossi <caio_gossi@hotmail.com>
2026-06-06 23:19:44 -0300

    feat: criar componentes Login e Main e adicionar routing

--- Frontend/vaccination-system-frontend/src/app/app.routes.ts
+++ Frontend/vaccination-system-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Main } from './pages/main/main';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'login', component: Login },
+    { path: 'home', component: Main },
+    { path: '**', redirectTo: 'home' }
+];
